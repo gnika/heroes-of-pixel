4f65b62bb4dfd324751c9e3678ac56c40d12f431
jeremie <jeremie@debian>
2017-11-16 13:58:08 +0100

    refacto

---
+++ assets/js/building.js
@@ -0,0 +1,48 @@
+function Building(map, x, y, row, col, life, name, hourstart, batiment, caracteristique) {
+    this.map = map;
+    this.life = life;
+    this.name = name;
+     this.x = x;
+     this.y = y;
+    this.col = col;
+    this.row = row;
+    this.hourstart = hourstart;
+    this.batiment = batiment;
+    this.caracteristique=caracteristique;
+    //si batiment envoit des projectiles
+    this.xDelta=0;
+    this.yDelta=0;
+    this.cible=0;
+    this.cibleMouvante=0;
+    
+    this.calculPortee = function(x, y, portee, rowMonstre, colMonstre)
+    { 
+    
+        xLimiteMoins = x - portee;
+        xLimitePlus = x + portee;
+        
+        yLimiteMoins = y - portee;
+        yLimitePlus = y + portee;
+        
+        if(xLimiteMoins<0)
+            xLimiteMoins = 0;
+        if(yLimiteMoins<0)
+            yLimiteMoins = 0;
+        
+        var porteeX = [x];
+        var porteeY = [y];
+        
+        for(var i=1;i<=portee;i++){
+            porteeX.push(x-i); 
+            porteeX.push(x+i); 
+            porteeY.push(y-i); 
+            porteeY.push(y+i); 
+        }
+        
+        if(porteeX.includes(rowMonstre) && porteeY.includes(colMonstre))
+            return true;
+        else 
+            return false;
+        
+    }
+ }
